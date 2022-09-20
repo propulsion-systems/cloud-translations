30377ab6efd7f5cdd8e89b12cf4e77521c4ea908
Monty <jonah@propulsionsystems.be>
2022-09-20 12:38:23 +0200

    Refactored & added languages fetch

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import crowdin from '@crowdin/crowdin-api-client'
 import { XMLParser } from 'fast-xml-parser'
-import fs from 'node:fs'
 import fsp from 'node:fs/promises'
-import crypto from 'node:crypto';
+import crypto from 'node:crypto'
 
-const project_id = 515350
-const languages = ['nl', 'fr']
+const project = process.env.CROWDIN_PROJECT
 
-let checksums = {};
-
-const { translationsApi } = new crowdin.default({
+const { translationsApi, projectsGroupsApi } = new crowdin.default({
   token: process.env.CROWDIN_TOKEN
 })
 
@@ -17,37 +13,35 @@ const parser = new XMLParser({
   ignoreAttributes: false
 })
 
-createFolders();
-getFiles();
+await fsp.mkdir('dist/languages', {
+  recursive: true
+})
 
-function createFolders() {
-    if(!fs.existsSync("./lib/languages")) {
-        fs.mkdirSync('./lib', { recursive: true });
-        fs.mkdirSync('./lib/languages', { recursive: true });
-    }
-}
+const { data: { targetLanguageIds: languages } } = await projectsGroupsApi.getProject(project)
 
-async function getFiles(){
-    await Promise.all(languages.map(async (language) => {
-        const { data: { url } } = await translationsApi.exportProjectTranslation(project_id, {
-            targetLanguageId: language,
-            format: 'xliff'
-        })
-    
-        const response = await fetch(url)
-        const xml = await response.text()
-    
-        const { xliff: { file: { body } } } = parser.parse(xml)
-    
-        const translations = {}
-    
-        for (const unit of body['trans-unit']) {
-            translations[unit['@_resname']] = unit.target['#text']
-        }
-    
-        checksums[language] = crypto.createHash('md5').update(JSON.stringify(translations, null, '\t')).digest("hex");
-    
-        fsp.writeFile(`lib/languages/${language}.json`, JSON.stringify(translations, null, '\t'))
-    }));
-    fsp.writeFile(`lib/languages/translations.json`, JSON.stringify(checksums, null, '\t'))
-}
+const available = await Promise.all(languages.map(async (language) => {
+  const { data: { url } } = await translationsApi.exportProjectTranslation(project, {
+    targetLanguageId: language,
+    format: 'xliff'
+  })
+
+  const response = await fetch(url)
+  const xml = await response.text()
+
+  const { xliff: { file: { body } } } = parser.parse(xml)
+
+  const translations = {}
+
+  for (const unit of body['trans-unit']) {
+    translations[unit['@_resname']] = unit.target['#text']
+  }
+
+  const json = JSON.stringify(translations, null, '\t')
+  const hash = crypto.createHash('md5').update(json).digest("hex")
+
+  await fsp.writeFile(`dist/languages/${language}.json`, json)
+
+  return { [language]: hash }
+}))
+
+await fsp.writeFile(`dist/available.json`, JSON.stringify(available))
